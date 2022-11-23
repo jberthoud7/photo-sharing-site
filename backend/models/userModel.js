05da29515e759ff2b55eab3ed42470ba6340bc1c
jamesberthoud <jberthoud@wustl.edu>
2022-11-23 13:27:45 -0600

    hash pswds

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema({
     username: {
@@ -15,6 +16,31 @@ const userSchema = mongoose.Schema({
     }
 }, {
     timestamps: true
+})
+
+
+userSchema.pre("save", function(next) {
+    const user = this
+
+    if(this.isNew){
+        bcrypt.genSalt(10, function (saltError, salt){
+            if(saltError){
+                return next(saltError)
+            } else{
+                bcrypt.hash(user.hashed_pswd, salt, function(hashError, hash){
+                    if(hashError){
+                        return next(hashError)
+                    }
+
+                    user.hashed_pswd = hash
+                    next()
+                })
+            }
+        })
+    }
+    else{
+        return next()
+    }
 })
 
 //mongoose.model("COLLECTION NAME". ___Schema)
