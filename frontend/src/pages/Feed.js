d59debb42418e130d3a985274da97030c15c43f9
Carolina Alisio <calisio@wustl.edu>
2022-11-30 11:53:55 -0600

    post loads (preliminary), multiple posts loading WIP

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,63 +1,64 @@
-//import {useState} from "react";
+import React from "react";
 import NavBar from "../components/NavBar"
 import Post from "../components/Post";
 import classes from "./pagesStyles/Feed.module.css"
 import axios from 'axios';
 
-const DUMMY_DATA = [
-    {
-        id: '1',
-        image: 'https://s3.us-east-1.amazonaws.com/florence-2019/images/_1200x630_crop_center-center_82_none/chick1.jpg?mtime=20201009125544',
-        caption: 'this is the first post',
-        user_id: 'AAA111AAA',
-        username: 'user1',
-        likes: 11,
-        comments: [
-            {comment:"good post", user:"user2"},
-            {comment:"bad post", user:"user3"}
-        ]
-    },
-    {
-        id: '2',
-        image: 'https://brandlogos.net/wp-content/uploads/2022/02/chick-fil-a-logo-brandlogos.net_-512x512.png',
-        caption: 'this is the second post',
-        user_id: '222BBB222',
-        username: 'user2',
-        likes: 22,
-        comments: [
-            {comment:"good post", user:"user1"},
-            {comment:"bad post", user:"user3"}
-        ]
+class Feed extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        id: '',
+        image: '',
+        caption:'',
+        likes: 0
+      };
     }
-]
-
-
-function Feed(props){
-
-    axios.get('http://localhost:3000/getPost')
-        .then((res) => {
-            console.log("success");
-            console.log(res.data);
-        })
-        .catch(err => {
-            console.log("error");
-            console.log(err);
-        });
-
-    return(
+  
+    componentDidMount() {
+        axios.get('http://localhost:3000/getPost')
+            .then((res) => {
+                console.log("success");
+                console.log(res.data);
+                this.setState({
+                    id: res.data.data[0].id,
+                    image: res.data.data[0].image,
+                    caption: res.data.data[0].caption,
+                    likes: res.data.data[0].likes
+                });
+            })
+            .catch(err => {
+                console.log("error");
+                console.log(err);
+            });
+    }
+    // componentDidUpdate() {
+    //     axios.get('http://localhost:3000/getPost')
+    //         .then((res) => {
+    //             console.log("success");
+    //             console.log(res.data);
+    //         })
+    //         .catch(err => {
+    //             console.log("error");
+    //             console.log(err);
+    //         });
+    // }
+  
+    render() {
+      return (
         <div>
             <NavBar />
             <ul className={classes.list}>
-                {DUMMY_DATA.map(post => (
                 <Post 
-                    key={post.id} 
-                    img={post.image}
-                    caption={post.caption}
-                    username={post.username}
-                />))}
+                    key={this.state.id} 
+                    img={this.state.image}
+                    caption={this.state.caption}
+                    username="1234"
+                />
             </ul>
         </div>
-    )
-}
+      );
+    }
+  }
 
-export default Feed
+  export default Feed;
